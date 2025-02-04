7ece88686efbbe03f873935bfb3befd21d554c44
MicroBlock <66859419+std-microblock@users.noreply.github.com>
2025-02-04 17:56:59 +0800

    fix: animation

--- src/shell/contextmenu/menu_widget.cc
+++ src/shell/contextmenu/menu_widget.cc
@@ -399,6 +399,8 @@ void mb_shell::menu_item_normal_widget::reset_appear_animation(float delay) {
   opacity->reset_to(0);
   opacity->animate_to(255);
   this->y->progress = 1;
+  this->y->easing = ui::easing_type::mutation;
+
   this->x->reset_to(-20);
   this->x->animate_to(0);
 }
@@ -459,7 +461,7 @@ void mb_shell::menu_widget::reset_animation(bool reverse) {
   // the show duration for the menu should be within 200ms
   float delay = std::min(200.f / children.size(), 30.f);
 
-  if (config::current->context_menu.reverse_if_open_to_up)
+  if (config::current->context_menu.reverse_if_open_to_up && reverse)
     reverse = !reverse;
 
   for (size_t i = 0; i < children.size(); i++) {
