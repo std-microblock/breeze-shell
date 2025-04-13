f6dd39f05927a2e53a9d3c8aa908d5161c2ec76a
MicroBlock <66859419+std-microblock@users.noreply.github.com>
2025-04-13 14:12:27 +0800

    feat(ui): experimental touchscreen support
    close #95

--- dependencies/glfw.lua
+++ dependencies/glfw.lua
@@ -3,4 +3,4 @@ package("breeze-glfw")
 
     set_urls("https://github.com/breeze-shell/glfw.git")
 
-    add_versions("2025.02.16", "07f5b129955039e0f401fc24be41b60e168e0bbb")
+    add_versions("2025.04.13", "71513247ae3cc7eca66ca88f0789c64b5e693115")
