88f9736d296c0dd7df14c0e693075e032493afa9
MicroBlock <66859419+std-microblock@users.noreply.github.com>
2025-06-14 18:16:30 +0800

    chore: bump version

--- xmake.lua
+++ xmake.lua
@@ -1,7 +1,7 @@
 set_project("shell")
 set_policy("compatibility.version", "3.0")
 set_policy("package.requires_lock", true)
-local version = "0.1.24"
+local version = "0.1.25"
 
 set_languages("c++2b")
 set_warnings("all") 
