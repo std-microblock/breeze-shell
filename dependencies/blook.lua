573055c7a4c9b837b1896ac011d7861ccdccb0bb
ColdLemonTea <coldlemontea233@gmail.com>
2025-09-14 14:49:25 +0800

    fix: update blook version and clean up dependencies

--- dependencies/blook.lua
+++ dependencies/blook.lua
@@ -4,8 +4,6 @@ package("blook")
 
     add_urls("https://github.com/std-microblock/blook.git")
 
-    add_versions("2025.09.14", "966cd3269d055f04b725e40fc0670b8c212bd97a")
-
     add_configs("shared", {description = "Build shared library.", default = false, type = "boolean", readonly = true})
 
     if is_plat("windows") then
